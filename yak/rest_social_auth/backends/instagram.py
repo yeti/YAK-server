e1f7200691e99679d4b6a74be8d7198d41e2f79b
Baylee Feore <baylee@yeti.co>
2014-12-11 16:49:27 -0800

    Add instagram extra data

--- yak/rest_social_auth/backends/instagram.py
+++ yak/rest_social_auth/backends/instagram.py
@@ -1,1 +1,16 @@
-__author__ = 'baylee'
+from social.backends.instagram import InstagramOAuth2
+from yak.rest_social_auth.backends.base import ExtraDataAbstractMixin
+
+
+class Instagram(ExtraDataAbstractMixin, InstagramOAuth2):
+    @staticmethod
+    def save_extra_data(response, user):
+        if response['data']['bio']:
+            user.about = response['bio']
+
+        user.save()
+
+    @staticmethod
+    def get_profile_image(strategy, details, response, uid, user, social, is_new=False, *args, **kwargs):
+        image_url = response['data']['profile_picture']
+        return image_url
