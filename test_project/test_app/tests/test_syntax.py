fefab6d2c142c64443f3670c6dfc5a4112fbc38d
Baylee Feore <baylee@yeti.co>
2015-01-29 16:35:42 -0800

    Make syntax test reusable

--- test_project/test_app/tests/test_syntax.py
+++ test_project/test_app/tests/test_syntax.py
@@ -1,41 +1,6 @@
-from django.test import TestCase
-from flake8.engine import get_style_guide
-import flake8.main
-import sys
-from test_project import settings
+from yak.rest_core.test import YAKSyntaxTest
 
 
-class SyntaxTest(TestCase):
+class SyntaxTest(YAKSyntaxTest):
     def test_syntax(self):
-        """
-        From flake8
-        """
-        packages = settings.PACKAGES_TO_TEST
-
-        # Prepare
-        flake8_style = get_style_guide(parse_argv=True, config_file=flake8.main.DEFAULT_CONFIG)
-        options = flake8_style.options
-
-        if options.install_hook:
-            from flake8.hooks import install_hook
-            install_hook()
-
-        # Save to file for later printing instead of printing now
-        old_stdout = sys.stdout
-        sys.stdout = out = open('syntax_output', 'w+')
-
-        # Run the checkers
-        report = flake8_style.check_files(paths=packages)
-
-        sys.stdout = old_stdout
-
-        # Print the final report
-        options = flake8_style.options
-        if options.statistics:
-            report.print_statistics()
-        if options.benchmark:
-            report.print_benchmark()
-        if report.total_errors:
-            out.close()
-            with open("syntax_output") as f:
-                self.fail("{0} Syntax warnings!\n\n{1}".format(report.total_errors, f.read()))
+        self.check_syntax()
--- yak/rest_core/test.py
+++ yak/rest_core/test.py
@@ -2,6 +2,10 @@ import json
 from rest_framework.test import APITestCase
 from django.conf import settings
 from yak.settings import yak_settings
+from django.test import TestCase
+from flake8.engine import get_style_guide
+import flake8.main
+import sys
 
 
 class APITestCaseWithAssertions(APITestCase):
@@ -402,3 +406,39 @@ class SchemaTestCase(APITestCaseWithAssertions):
                 obj_to_update.thumbnail.file.read(),
                 open(settings.PROJECT_ROOT + path_to_thumbnail, 'r').read()
             )
+
+
+class YAKSyntaxTest(TestCase):
+    def check_syntax(self):
+        """
+        From flake8
+        """
+        packages = settings.PACKAGES_TO_TEST
+
+        # Prepare
+        flake8_style = get_style_guide(parse_argv=True, config_file=flake8.main.DEFAULT_CONFIG)
+        options = flake8_style.options
+
+        if options.install_hook:
+            from flake8.hooks import install_hook
+            install_hook()
+
+        # Save to file for later printing instead of printing now
+        old_stdout = sys.stdout
+        sys.stdout = out = open('syntax_output', 'w+')
+
+        # Run the checkers
+        report = flake8_style.check_files(paths=packages)
+
+        sys.stdout = old_stdout
+
+        # Print the final report
+        options = flake8_style.options
+        if options.statistics:
+            report.print_statistics()
+        if options.benchmark:
+            report.print_benchmark()
+        if report.total_errors:
+            out.close()
+            with open("syntax_output") as f:
+                self.fail("{0} Syntax warnings!\n\n{1}".format(report.total_errors, f.read()))
